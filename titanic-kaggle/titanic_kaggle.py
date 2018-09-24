ba03950e2458498daac8cebfe0e60d0ae17d8589
Sinjoy Saha <sinjoysaha555@gmail.com>
2018-09-25 00:36:10 +0530

    Added Fare_categories to Titanic Model

--- titanic-kaggle/titanic_kaggle.py
+++ titanic-kaggle/titanic_kaggle.py
@@ -8,8 +8,12 @@ data = pd.read_csv('train.csv')
 print(data.head(10))
 print(data.columns)
 
-print(data.Parch.value_counts())
-data.pivot_table(index="Parch", values="Survived").plot.bar()
+
+survived = data[data["Survived"] == 1]
+died = data[data["Survived"] == 0]
+survived["Fare"].plot.hist(alpha=0.5, color='red', bins=30, rwidth=0.8)
+data["Fare"].plot.hist(alpha=0.5, color='green', bins=30, rwidth=0.8)
+plt.legend(['Survived', 'Total'])
 plt.show()
 
 data['Age'] = data["Age"].fillna(-0.5)
@@ -17,6 +21,13 @@ data['Age_categories'] = pd.cut(data["Age"], [-1, 0, 5, 12, 18, 35, 60, 100], la
 
 data['Parch_categories'] = pd.cut(data["Parch"], [-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 10],
                                   labels=["0", '1', '2', '3', '4', '5', '6above'])
+data['Fare_categories'] = pd.cut(data["Fare"], [-0.5, 20, 40, 60, 80, 100, 200, 300, 1000],
+                                 labels=["20", '40', '60', '80', '100', '200', '300', '300above'])
+
+
+survived = data[data["Survived"] == 1]
+survived["Fare_categories"].value_counts().plot.bar()
+plt.show()
 
 
 def create_dummies(df, column_name):
@@ -31,6 +42,7 @@ data = create_dummies(data, "Age_categories")
 data = create_dummies(data, "Embarked")
 data = create_dummies(data, "SibSp")
 data = create_dummies(data, "Parch_categories")
+data = create_dummies(data, "Fare_categories")
 
 print(data.columns)
 
@@ -40,8 +52,12 @@ columns = ['Pclass_1', 'Pclass_2', 'Pclass_3', 'Sex_female', 'Sex_male',
            'Age_categories_Young_Adult', 'Age_categories_Adult',
            'Age_categories_Senior', 'Embarked_C',
            'Embarked_Q', 'Embarked_S', 'SibSp_0', 'SibSp_1', 'SibSp_2',
-           'SibSp_3', 'SibSp_4', 'SibSp_5', 'SibSp_8', 'Parch_categories_0', 'Parch_categories_1',
-           'Parch_categories_2', 'Parch_categories_3', 'Parch_categories_4', 'Parch_categories_5', 'Parch_categories_6above']
+           'SibSp_3', 'SibSp_4', 'SibSp_5', 'SibSp_8', 'Parch_categories_0',
+           'Parch_categories_1', 'Parch_categories_2', 'Parch_categories_3',
+           'Parch_categories_4', 'Parch_categories_5', 'Parch_categories_6above',
+           'Fare_categories_20', 'Fare_categories_40', 'Fare_categories_60',
+           'Fare_categories_80', 'Fare_categories_100', 'Fare_categories_200', 'Fare_categories_300',
+           'Fare_categories_300above']
 
 y = data['Survived']
 X = data[columns]
@@ -56,19 +72,22 @@ print(acc)
 
 
 test_data = pd.read_csv('test.csv')
-print(test_data.Parch.value_counts())
 test_data['Age'] = test_data["Age"].fillna(-0.5)
 test_data['Age_categories'] = pd.cut(test_data["Age"], [-1, 0, 5, 12, 18, 35, 60, 100], labels=["Missing", 'Infant', "Child", 'Teenager', "Young_Adult", 'Adult', 'Senior'])
 test_data['Parch_categories'] = pd.cut(test_data["Parch"], [-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 10],
                                        labels=["0", '1', '2', '3', '4', '5', '6above'])
+test_data['Fare'] = data["Fare"].fillna(32.2)
+test_data['Fare_categories'] = pd.cut(test_data["Fare"], [-0.5, 20, 40, 60, 80, 100, 200, 300, 1000],
+                                      labels=["20", '40', '60', '80', '100', '200', '300', '300above'])
+
 
 test_data = create_dummies(test_data, "Pclass")
 test_data = create_dummies(test_data, "Sex")
 test_data = create_dummies(test_data, "Age_categories")
 test_data = create_dummies(test_data, "Embarked")
 test_data = create_dummies(test_data, "SibSp")
-test_data = create_dummies(test_data, "Parch")
 test_data = create_dummies(test_data, "Parch_categories")
+test_data = create_dummies(test_data, "Fare_categories")
 
 print(test_data.columns)
 
